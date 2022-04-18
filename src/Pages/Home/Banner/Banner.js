e3f0c125d04f06e6575a701e943db35c2d9c1a7d
sajidur10 <msrrahatbd1@gmail.com>
2022-04-18 23:53:16 +0600

    banar fix

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,9 +8,9 @@ import banner3 from '../../../images/banner/banner3.jpg';
 const Banner = () => {
     const [index, setIndex] = useState(0);
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item>
@@ -19,10 +19,7 @@ const Banner = () => {
                     src={banner1}
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -31,10 +28,7 @@ const Banner = () => {
                     alt="Second slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -43,12 +37,7 @@ const Banner = () => {
                     alt="Third slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
+
             </Carousel.Item>
         </Carousel>
     );
