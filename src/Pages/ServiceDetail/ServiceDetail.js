9ee80bbac59f8e9c6a78c607de6c58d3648d0132
sajidur10 <msrrahatbd1@gmail.com>
2022-04-18 22:57:46 +0600

    bug fixed

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -3,9 +3,13 @@ import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
+    console.log(serviceId)
     return (
         <div>
-            <h2>Welcome to detail: {serviceId}</h2>
+            <div className='text-center'>
+                <h2>Welcome to detail: {serviceId}</h2>
+            </div>
+
             <div className='text-center'>
                 <Link to="/checkout">
                     <button className='btn btn-primary'>Proceed Checkout</button>
